dab747d294d0064dcebc712db6d47844ace0924f
chorome <odk.mmmm@gmail.com>
2022-11-19 19:03:59 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -l
 
+echo "=================args================="
+
 echo "Hello $1"
 echo "file_path $2"
 echo "field_value $3"
 
+echo "=================ls -la================="
+
 pwd
 ls -la
+
+echo "=================time================="
 
 time=$(date)
 echo "::set-output name=time::$time"
